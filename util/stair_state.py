bb0c754b3a7a50f635913caeee8bff615dc91059
Mateus Furquim <furquim.axs@gmail.com>
2016-05-13 14:05:15 -0300

    Calculando todos os 326 estados possíveis para a escada.

---
+++ util/stair_state.py
@@ -0,0 +1,16 @@
+from itertools import *
+
+class Counter(dict):
+    def __missing__(self, key):
+        return 0
+
+c = Counter()
+for n in range(0,6):
+    a = permutations('RGBYP', n)
+    for it in a:
+        c[it] += 1
+
+i = 0
+for b in c:
+    print str(i) + ": " + str(b)
+    i += 1
