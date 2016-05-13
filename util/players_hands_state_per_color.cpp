b176f946412084ceec6d80002221e565fe0734a2
Mateus Furquim <furquim.axs@gmail.com>
2016-05-13 11:54:50 -0300

    Calculando todos os 3003 estados possíveis para cada cor.

---
+++ util/players_hands_state_per_color.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <type_traits>
+
+int main(){
+    unsigned int counter = 0;
+    for (size_t n = 0; n < 0x7c01; n++) {
+        std::bitset<15> b(n);
+        if (b.count() == 5) {
+            std::cout << counter << ": " << b.to_string() << std::endl;
+            counter++;
+        }
+    }
+}
